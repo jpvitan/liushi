ef8aaa0919d572646f8caf76a20c779af9a8de29
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-06-23 00:06:08 +0800

    Finish model module

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ Copyright © 2021 Justine Paul Sanchez Vitan. All rights reserved.
 
 from tensorflow.keras import Sequential
 from tensorflow.keras import layers
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 
 def create_model():
@@ -27,5 +28,29 @@ def create_model():
     model.add(layers.Dense(units=15, activation='softmax'))
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+def train_model(data, epochs, checkpoint_location):
+    model = create_model()
+
+    training_feature = data[0]
+    training_label = data[1]
+    validation_feature = data[2]
+    validation_label = data[3]
+
+    callback = ModelCheckpoint(checkpoint_location, save_weights_only=True, verbose=1)
+
+    history = model.fit(training_feature, training_label, validation_data=(validation_feature, validation_label),
+                        epochs=epochs, callbacks=[callback], verbose=1).history
+
+    return model
+
+
+def load_model(checkpoint_location):
+    model = create_model()
+
+    model.load_weights(checkpoint_location)
 
     return model

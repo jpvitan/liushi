72a3380d836bc2c55ada7cf0997ace2714c826b8
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-06-25 03:06:23 +0800

    Remove prediction warning

--- workspace.py
+++ workspace.py
@@ -6,6 +6,8 @@ Created by Justine Paul Sanchez Vitan.
 Copyright © 2021 Justine Paul Sanchez Vitan. All rights reserved.
 """
 
+import numpy as np
+
 import data
 import model
 
@@ -14,16 +16,16 @@ checkpoint_location = 'data/checkpoint/train.ckpt'
 
 def train():
     img_data = data.Data()
-    model.train_model(img_data.validation_split(0.2), 2, checkpoint_location)
+    model.train_model(img_data.validation_split(0.2), 3, checkpoint_location)
 
 
 def predict(img_location):
     cnn = model.load_model(checkpoint_location)
 
     img_ndarray = data.img_to_ndarray(img_location).reshape(-1, 64, 64, 1)
-    prediction = data.denormalize_value(cnn.predict_classes(img_ndarray)[0])
+    prediction = data.denormalize_value(np.argmax(cnn.predict(img_ndarray)))
 
     data.inspect_img(img_ndarray, 'Prediction: ' + str(prediction))
 
 
-predict('data/test/test6.jpg')
+predict('data/test/test1.jpg')

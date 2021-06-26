27ed07cc950492c73c0b9f6dc787cee29d0af1b2
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2021-06-26 23:22:07 +0800

    Update model

--- model.py
+++ model.py
@@ -15,12 +15,12 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 def create_model():
     model = Sequential()
 
-    model.add(layers.Conv2D(filters=16, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
+    model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
                             input_shape=(64, 64, 1)))
     model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
-    model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-    model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
     model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
     model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
     model.add(layers.Flatten())
     model.add(layers.Dense(units=1024, activation='relu'))
